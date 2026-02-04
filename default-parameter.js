fae1a281fd3797d3bc268eefc146e73b8aacdbae
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2026-02-04 18:59:30 +0600

    function

---
+++ default-parameter.js
@@ -0,0 +1,8 @@
+
+//(num1, num2) = parameter
+function sum(num1, num2){
+    const total = num1 + num2;
+    console.log(total);
+}
+
+sum(20,50) //arguments
--- let-const.js
+++ let-const.js
@@ -7,3 +7,16 @@ console.log(countryName);
 let price = 100;
 price = 160;
 console.log(price)
+
+
+// 
+const charge = 50;
+// charge = 45;
+
+const dress = ['shirt', 'pant', 'jacket'];
+dress.push('maplar')
+console.log(dress);
+
+const student = {name: 'mohiuddin', marks: 60};
+student.marks = 80;
+console.log(student);
